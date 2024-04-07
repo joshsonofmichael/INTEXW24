24f3002ad0e08da9a1e32c626bf20464f4910be9
joshsonofmichael <107602793+joshsonofmichael@users.noreply.github.com>
2024-04-07 00:02:17 -0600

    generated the user stuff but haven't deleted the Accounts database (so there are still two connection strings)

--- intexw24/Program.cs
+++ intexw24/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using intexw24.Data;
+using intexw24.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
